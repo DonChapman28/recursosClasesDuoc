cf8500a46c25178cf4bf88131601037126703e3c
eSponjosa <105675474+DonChapman28@users.noreply.github.com>
2023-09-25 19:43:33 -0300

    Add files via upload

---
+++ EjercicioPracticaExamen-main/JavaEjemploPrueba123456/src/Modelo/Mecanico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+
+public class Mecanico extends Persona implements IOperaciones {
+    @Override
+    public void limpiar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String imprimirDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ EjercicioPracticaExamen-main/JavaEjemploPrueba123456/src/Modelo/Vehiculo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+
+public class Vehiculo implements IOperaciones {
+    @Override
+    public void limpiar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String imprimirDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
